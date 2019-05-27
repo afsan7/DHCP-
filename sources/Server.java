649b4ad5ae6626357d1ec23ff7d92ef6f1062190
Afsan Hossain <afsanh42@gmail.com>
2019-05-27 19:33:34 +0530

    Add files via upload

---
+++ sources/Server.java
@@ -0,0 +1,10 @@
+import DHCPServer.DHCPServer;
+
+public class Server {
+	public static void main(String[] args)
+	{
+		String serverIP = args[0];
+		DHCPServer server = new DHCPServer(serverIP);
+		server.run();
+	}
+}
